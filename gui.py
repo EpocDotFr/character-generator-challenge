8f0fc0984e2e9dc5387d841e821bc6209a6d30be
EpocDotFr <contact@epoc.fr>
2018-02-19 18:01:38 +0100

    Create radio button GUI element

--- gui.py
+++ gui.py
@@ -43,3 +43,54 @@ class Button(pygame.sprite.Sprite):
         self.on_click(self)
 
         return True
+
+
+class RadioButton(pygame.sprite.Sprite):
+    """A radio button."""
+    def __init__(self, images, rects, value):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.images = images
+        self.rects = rects
+        self.value = value
+        self.selected = False
+
+    @property
+    def selected(self):
+        return self._selected
+
+    @selected.setter
+    def selected(self, selected):
+        self._selected = selected
+
+        if self._selected:
+            self.image = self.images['normal']
+            self.rect = self.rects['normal']
+        else:
+            self.image = self.images['selected']
+            self.rect = self.rects['selected']
+
+
+class RadioButtons(pygame.sprite.Group):
+    """A collection of radio buttons."""
+    def __init__(self, on_select):
+        pygame.sprite.Group.__init__(self)
+
+        self.on_select = on_select
+
+    def event_handler(self, event):
+        """Handle the PyGame event and run the appropriate callbacks if related to this radio buttons collection."""
+        if self.on_select is None or event.type != pygame.MOUSEBUTTONDOWN:
+            return False
+
+        for radio_button in self.sprites():
+            if radio_button.rect.collidepoint(event.pos):
+                self.on_select(radio_button)
+
+                radio_button.selected = True
+
+                return True
+            # else:
+            #    radio_button.selected = False
+
+        return False
